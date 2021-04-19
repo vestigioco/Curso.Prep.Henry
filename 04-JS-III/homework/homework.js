e5816195f7abc16fc82d0e14143f1133abdae884
Mauricio Henao <mauricio@vestigio.ec>
2021-04-19 16:14:17 -0500

    finalizado homework 04-JS-III

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -23,14 +23,19 @@ function incrementarPorUno(array) {
 	// Aumenta cada entero por 1
 	// y devuelve el array
 	// Tu código:
+	var nuevoArray = [];
+	for (var i = 0; i < array.length; i++) {
+		nuevoArray[i] = array[i] + 1;
+	}
+	return nuevoArray;
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
 	// Añade el "elemento" al final del array
 	// y devuelve el array
 	// Tu código:
-	var elemento = 1;
 	array.push(elemento);
+	return array;
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
@@ -38,6 +43,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
 	// y devuelve el array
 	// Pista: usa el método `.unshift`
 	// Tu código:
+	array.unshift(elemento);
+	return array;
 }
 
 function dePalabrasAFrase(palabras) {
@@ -46,41 +53,79 @@ function dePalabrasAFrase(palabras) {
 	// con espacios entre cada palabra
 	// Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
 	// Tu código:
+	return palabras.join(' ');
 }
 
 function arrayContiene(array, elemento) {
 	// Comprueba si el elemento existe dentro de "array"
 	// Devuelve "true" si está, o "false" si no está
 	// Tu código:
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] === elemento) {
+			return true;
+		}
+	}
+	return false;
 }
 
 function agregarNumeros(numeros) {
 	// "numeros" debe ser un arreglo de enteros (int/integers)
 	// Suma todos los enteros y devuelve el valor
 	// Tu código:
+	var suma = 0;
+	for (var i = 0; i < numeros.length; i++) {
+		suma = suma + numeros[i];
+	}
+	return suma;
 }
 
 function promedioResultadosTest(resultadosTest) {
 	// "resultadosTest" debe ser una matriz de enteros (int/integers)
 	// Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
 	// Tu código:
+
+	var suma = 0;
+	for (var i = 0; i < resultadosTest.length; i++) {
+		suma = suma + resultadosTest[i];
+	}
+	return suma / resultadosTest.length;
 }
 
 function numeroMasGrande(numeros) {
 	// "numeros" debe ser una matriz de enteros (int/integers)
 	// Devuelve el número más grande
 	// Tu código:
+	var maximo = numeros[0];
+	for (i = 1; i < numeros.length; i++) {
+		if (numeros[i] > maximo) {
+			maximo = numeros[i];
+		}
+	}
+	return maximo;
 }
 
 function multiplicarArgumentos() {
 	// Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
 	// Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
 	// Escribe tu código aquí:
+	if (arguments.length < 1) return 0;
+	var multiplicacion = 1;
+	for (var i = 0; i < arguments.length; i++) {
+		multiplicacion = multiplicacion * arguments[i];
+	}
+	return multiplicacion;
 }
 
 function cuentoElementos(arreglo) {
 	//Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
 	//Escribe tu código aquí
+	var cantidad = 0;
+	for (var i = 0; i < arreglo.length; i++) {
+		if (arreglo[i] > 19) {
+			cantidad++;
+		}
+	}
+	return cantidad;
 }
 
 function diaDeLaSemana(numeroDeDia) {
@@ -88,18 +133,33 @@ function diaDeLaSemana(numeroDeDia) {
 	//Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
 	//si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario.
 	//Escribe tu código aquí
+	if (numeroDeDia === 1 || numeroDeDia === 7) {
+		return 'Es fin de semana';
+	}
+	return 'Es dia Laboral';
 }
 
 function empiezaConNueve(n) {
 	//Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero
 	//inicia con 9 y false en otro caso.
 	//Escribe tu código aquí
+	var numero = n.toString();
+	if (numero.charAt(0) === '9') {
+		return true;
+	}
+	return false;
 }
 
 function todosIguales(arreglo) {
 	//Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
 	//retornar true, caso contrario retornar false.
 	//Escribe tu código aquí
+	for (var i = 0; i < arreglo.length - 1; i++) {
+		if (arreglo[i] !== arreglo[i + 1]) {
+			return false;
+		}
+	}
+	return true;
 }
 
 function mesesDelAño(array) {
@@ -107,12 +167,30 @@ function mesesDelAño(array) {
 	// "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
 	//Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
 	// Tu código:
+	nuevoArray = [];
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] === 'Enero' || array[i] === 'Marzo' || array[i] === 'Noviembre') {
+			nuevoArray.push(array[i]);
+		}
+	}
+	if (nuevoArray.length < 3) {
+		return 'No se encontraron los meses pedidos';
+	} else {
+		return nuevoArray;
+	}
 }
 
 function mayorACien(array) {
 	//La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
 	//valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
 	// Tu código:
+	nuevoArray = [];
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] > 100 && array[i] <= 200) {
+			nuevoArray.push(array[i]);
+		}
+	}
+	return nuevoArray;
 }
 
 function breakStatement(numero) {
@@ -123,6 +201,21 @@ function breakStatement(numero) {
 	//devolver: "Se interrumpió la ejecución"
 	//Pista: usá el statement 'break'
 	// Tu código:
+	array = [];
+	suma = numero;
+	for (var i = 0; i < 10; i++) {
+		suma = suma + 2;
+		if (suma === i) {
+			break;
+		} else {
+			array.push(suma);
+		}
+	}
+	if (i < 10) {
+		return 'Se interrumpió la ejecución';
+	} else {
+		return array;
+	}
 }
 
 function continueStatement(numero) {
@@ -132,6 +225,17 @@ function continueStatement(numero) {
 	//Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
 	//Pista: usá el statement 'continue'
 	// Tu código:
+	array = [];
+	suma = numero;
+	for (var i = 0; i < 10; i++) {
+		if (i === 5) {
+			continue;
+		} else {
+			suma = suma + 2;
+			array.push(suma);
+		}
+	}
+	return array;
 }
 
 // No modificar nada debajo de esta línea
